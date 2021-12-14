67c91e103d6a5474b277ca7abf9cf05bb3fb7528
Stalin Crisanto <scrisanto@emagic.fin.ec>
2021-12-14 09:28:22 -0500

    questionnaires and evaluations start

--- pages/evaluations/evaluation-test.js
+++ pages/evaluations/evaluation-test.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PrivateLayout from '../../layouts/private_layout'
+import EvaluationTest from '../../modules/evaluation/EvaluationTest'
 import ContentHome from '../../modules/home/Content'
 
 export default function Home() {
     return (
         <PrivateLayout titlePage="Inicio">
-            <ContentHome />
+            <EvaluationTest />
         </PrivateLayout>
     )
 }
